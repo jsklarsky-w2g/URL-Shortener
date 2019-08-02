fb071f63d1d794c0501326321cbf681d8ccb6de1
Greg Roques <greg.roques@gmail.com>
2019-08-02 15:11:49 -0400

    Cypress Testing complete

--- Tests/cypress/integration/input.spec.js
+++ Tests/cypress/integration/input.spec.js
@@ -9,11 +9,18 @@ describe('Test URL Shortener',()=>{
     })
 
     it('should submit without a value and get an error message in the title field', ()=>{
-        // cy.get('[data-cy=input]').as('input')
-
-        // cy.get('@input').type('')
         cy.get('#root > div > div.Home_positioning__OAtY0 > form > div:nth-child(3) > button').click()
         cy.get('#root > div > div.Home_positioning__OAtY0 > form > p').contains('Oops...') 
     })
+
+    it('should input a URL and get a hyperlink in return for the same URL', ()=>{
+        cy.get('[data-cy=input]').as('input')
+
+        cy.get('@input').type('https://www.cnn.com')
+        cy.get('#root > div > div.Home_positioning__OAtY0 > form > div:nth-child(3) > button').click()
+        cy.get('#root > div > div.update_Modal__LVI5Q.update_ModalOpen__10h02').should('be.visible')
+        cy.get('#root > div > div.update_Modal__LVI5Q.update_ModalOpen__10h02 > div > p:nth-child(2) > a').should('have.attr','href').and('include', 'https://www.cnn.com')
+    })
+        
 })
 
