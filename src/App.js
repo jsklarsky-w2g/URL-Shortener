d91e4be1d4c356d3a975175571f1bb8e8d6d0f4b
Jeremy Sklarsky <jeremy.s@ware2go.co>
2019-08-10 10:53:18 -0400

    Install prettier and eslint

--- src/App.js
+++ src/App.js
@@ -1,130 +1,131 @@
-import React, { Component } from 'react';
-import appStyle from './App.module.css'
-import ResultModal from './components/modal';
-import axios from 'axios';
-import baseUrl from './FirebaseUrl';
+import React, { Component } from "react";
+import appStyle from "./App.module.css";
+import ResultModal from "./components/modal";
+import axios from "axios";
+import baseUrl from "./FirebaseUrl";
 
-import { createBrowserHistory } from 'history';
-const history = createBrowserHistory()
+import { createBrowserHistory } from "history";
+const history = createBrowserHistory();
 
 var randomstring = require("randomstring");
 
-class App extends Component{
-
+class App extends Component {
   state = {
-    currentUrls:{},
-    value: '',
+    currentUrls: {},
+    value: "",
     openModal: false,
     tinyURL: null,
-    title: 'Trim URL Below!'
-  }
+    title: "Trim URL Below!"
+  };
 
-  componentDidMount = async ()=> {
+  componentDidMount = async () => {
     await this.getURLs();
-    this.redirect()
-  }
+    this.redirect();
+  };
 
-  getURLs = async () =>{
+  getURLs = async () => {
     const res = await axios.get(`${baseUrl}.json`);
     const { data } = res;
-    Object.keys(data).map(each=>{
-      this.setState(prevState=>({
-        currentURL: prevState.currentUrls[data[each].hash] = data[each].originalUrl
-      }))
-    })
-  }
+    Object.keys(data).map(each => {
+      this.setState(prevState => ({
+        currentURL: (prevState.currentUrls[data[each].hash] =
+          data[each].originalUrl)
+      }));
+    });
+  };
 
-  redirect = ()=>{
-    if(window.location.pathname.replace(/[/]/,"").length >1 ){
-      let myUrl = ''
-      const param = window.location.pathname.replace(/[/]/,"")
-      Object.keys(this.state.currentUrls).find(currentURL=>{
-        if (currentURL === param){
-          myUrl = this.state.currentUrls[param]
+  redirect = () => {
+    if (window.location.pathname.replace(/[/]/, "").length > 1) {
+      let myUrl = "";
+      const param = window.location.pathname.replace(/[/]/, "");
+      Object.keys(this.state.currentUrls).find(currentURL => {
+        if (currentURL === param) {
+          myUrl = this.state.currentUrls[param];
         }
-      })
-      if(myUrl.length){
-        window.location.assign(myUrl)
+      });
+      if (myUrl.length) {
+        window.location.assign(myUrl);
       } else {
-        window.history.pushState(null,null,"/")
+        window.history.pushState(null, null, "/");
         this.setState({
-          title:'Not a valid URL. Create a new URL Below'
-        })
+          title: "Not a valid URL. Create a new URL Below"
+        });
       }
     }
-      
-  }
+  };
 
-  addToDatabase = (trim, full) =>{
+  addToDatabase = (trim, full) => {
     const myPost = {
       hash: trim,
       originalUrl: full
-    }
-    axios.post(`${baseUrl}.json`, myPost ) 
-      .then(async res=>{
+    };
+    axios
+      .post(`${baseUrl}.json`, myPost)
+      .then(async res => {
         await this.getURLs();
         this.setState({
-          openModal:true,
+          openModal: true,
           tinyURL: `localhost:3000/${trim}`
-        })
+        });
       })
-      .catch(err=>{
-        console.log(err)
+      .catch(err => {
+        console.log(err);
         this.setState({
-          title: 'Whoops...something went wrong'
-        })
-      })
-  }
+          title: "Whoops...something went wrong"
+        });
+      });
+  };
 
-  valueHandler = e =>{
+  valueHandler = e => {
     this.setState({
       value: e.target.value
-    })
-  }
+    });
+  };
 
-  createHash = () =>{
+  createHash = () => {
     let newHash = randomstring.generate({
       length: 12,
-      charset: 'alphabetic'
+      charset: "alphabetic"
     });
-    if(Object.keys(this.state.currentUrls).includes(newHash)){
-      this.createHash()
-    } else{
-      return newHash
+    if (Object.keys(this.state.currentUrls).includes(newHash)) {
+      this.createHash();
+    } else {
+      return newHash;
     }
-  }
+  };
 
-  submitHandler = e =>{
+  submitHandler = e => {
     e.preventDefault();
-    const currentUrls = this.state.currentUrls
-    const newUrl = this.state.value
-    if(newUrl === ''){
+    const currentUrls = this.state.currentUrls;
+    const newUrl = this.state.value;
+    if (newUrl === "") {
       this.setState({
-        title: 'Oops...you forgot to enter a URL'
-      })
-    }else{
-     if(Object.values(currentUrls).includes(newUrl)){
-        const link = Object.keys(currentUrls).find(url=>currentUrls[url] === newUrl)
+        title: "Oops...you forgot to enter a URL"
+      });
+    } else {
+      if (Object.values(currentUrls).includes(newUrl)) {
+        const link = Object.keys(currentUrls).find(
+          url => currentUrls[url] === newUrl
+        );
         this.setState({
           openModal: true,
           tinyURL: `localhost:3000/${link}`
-        })
+        });
       } else {
         const hash = this.createHash();
-        this.addToDatabase(hash, newUrl)
+        this.addToDatabase(hash, newUrl);
       }
     }
-  }
+  };
 
-  closeModal = () =>{
+  closeModal = () => {
     this.setState({
-        openModal: false
-    })
-  }
+      openModal: false
+    });
+  };
 
-
-  render(){
-    return(
+  render() {
+    return (
       <div className={appStyle.App}>
         <ResultModal
           show={this.state.openModal}
@@ -134,22 +135,23 @@ class App extends Component{
         />
         <h1>Greg's URL Shortener</h1>
         <div className={appStyle.positioning}>
-          <form className={appStyle.box} onSubmit={e=>this.submitHandler(e)}>
+          <form className={appStyle.box} onSubmit={e => this.submitHandler(e)}>
             <p>{this.state.title}</p>
             <div>
               <input
                 className={appStyle.inputText}
                 type="url"
-                placeholder='Paste URL Here'
+                placeholder="Paste URL Here"
                 value={this.state.value}
                 onChange={this.valueHandler}
                 data-cy="input"
               />
             </div>
             <div>
-              <button type="submit" className={appStyle.inputSubmit}>Submit</button>
+              <button type="submit" className={appStyle.inputSubmit}>
+                Submit
+              </button>
             </div>
-            
           </form>
         </div>
       </div>
